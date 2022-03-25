8fe4e914d7a79e2d0e74ae6aa070281610eecf39
Avinash <avinash1234sharma1@gmail.com>
2022-03-25 09:10:50 +0530

    completed algothrim to run the game

--- sudoku/src/App.js
+++ sudoku/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Hori from './Component/Hori';
+import {useState} from 'react';
 let sudoku =    [[
   0, 4, 0, 0, 0,
   0, 1, 7, 9
@@ -39,15 +40,104 @@ let sudoku =    [[
 ]
 ]
 
-function App() {
+// function Horizontal(y,x,num){
+//   for ( let i = 0; i < 9 && i != x; i++){
+//      if( sudoku_new[i][y] === num) {
 
- let alpha =  sudoku.map((row) => <Hori props={row} />);
- console.log(alpha);
+//      }
+//   }
+
+// }
+
+ 
+function App() {
+   let [sudoku_new,setSudoku] = useState(sudoku);
+
+  //  console.log(sudoku_new);
+
+   function changeInSudoku(col,row,num){
+     console.log(row,col);
+     num = +num
+    //  console.log(y,x,num)
+    //  console.log(sudoku_new);
+     if(!Horizontal(col,row,num)) alert("The number is present in horizontal");
+    else if(!Vertical(col,row,num)) alert("The number is present in Vertical");
+    else if(!blockOfThree(col,row,num)) alert("the number is present in 3 * 3");
+   else { 
+     sudoku_new[row][col] = num;
+   }
+
+//  console.log(sudoku_new[row][col]);  
+
+   }
+
+
+   function Horizontal(col,row,num){
+     console.log("-------------")
+    // console.log(sudoku_new[y][x]);
+    console.log('row :' + row)
+    for ( let i = 0; i < 9 ; i++){
+      // console.log(sudoku_new[row][i])
+       if( sudoku_new[row][i] === num && i != col) {
+          return false;
+       }
+    };
+    return true;
+  }
+
+  function Vertical(col,row,num){
+    for ( let i = 0; i < 9 ; i++){
+      // console.log(sudoku_new[i][col]);
+      if( sudoku_new[i][col] === num &&  i != row) {
+         return false;
+      }
+      
+   };
+   return true;
+  }
+
+   function blockOfThree(col,row,num){
+    //  console.log(col,row);
+   let  r = Math.floor(row/3)*3;
+   let  c = Math.floor(col/3)*3;
+     for ( let i = r; i < r+3; i++){
+       for ( let j = c ; j <  c + 3; j++){
+         console.log(sudoku_new[i][j]);
+         if(sudoku_new[i][j] === num && i !== row && j !== col){
+           return false;
+         }
+       }
+     }
+   return true;
+  }
+
+  function checkCorrectSudoku(){
+    console.log(1);
+    let row = sudoku_new.length; 
+    let col = sudoku_new[0].length;
+    for ( let i = 0; i < row; i++ ){
+      for ( let j = 0; j < col; j++){
+        if(sudoku_new[i][j] == 0){
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+ let alpha =  sudoku.map((row,i) => <Hori props={row} row={i} handleClick={ (e,x,y,num) => {
+   changeInSudoku(x,y,num);
+ }}/>);
+ //console.log(alpha);
  
   return (
     <div className="App">
     
       {alpha}
+      <button className="btn--sudoku" onClick={() => {
+        if(checkCorrectSudoku()) alert('You win');
+        else alert("Game is not completed yet");
+      }}> Check the sudoku</button>
       
      
     </div>
--- sudoku/src/Component/Hori.js
+++ sudoku/src/Component/Hori.js
@@ -1,26 +1,36 @@
 import React from 'react'
 import './Hori.css'
-function onDivChange(e){
-    const value = e.target.children;
-    console.log(value);
-   if( e.target.innerHTML !== '') console.log(e.target.innerHTML);
+function onDivChange(e,x,y,handleClick) {
+    const value = e.target.index;
+   // console.log(x,y);
+    // console.log(value);
+    if (e.target.innerHTML !== '') {
+        console.log(e.target.innerHTML);
+        handleClick(e,y,x,e.target.innerHTML);
+    }
 
     // setInterval(() => {
     //    console.log(e.target.innerHTML);
     // },1000)
 
 }
-    
-    
+
+
 
 function Hori(props) {
     //console.log(props);
-   console.log(props.props);
-   let arr = props.props;
-  let boxes = arr.map((item) => <div className='box' contentEditable="true" onInput ={onDivChange} >{item}</div>)
+  let handleClick = props.handleClick;
 
-    
-   
+
+    console.log(props.props);
+    console.log(props.row);
+    let arr = props.props;
+    let boxes = arr.map((item, col) => <div className='box' contentEditable="true" onInput={(e) => {
+        onDivChange(e,props.row,col,handleClick);
+    }}  >{item}</div>)
+
+
+
 
     console.log(boxes)
     return (
